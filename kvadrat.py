d6299e28ca6ac12cc84fa24737210780413138d8
Conny <kawohl.bentodesign@gmail.com>
2016-10-17 18:16:15 +0200

    adds randomizer

--- kvadrat.py
+++ kvadrat.py
@@ -1,5 +1,14 @@
 # kvadrat 0.0.1
 
+import random
+
+def randomizer():
+        x = random.randrange(0,100)
+	print x
+        return x
+
+randomizer()
+
 def rowbuilder():
 	for i in range(1,99):
 		if i % 3 == 0:
